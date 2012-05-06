138a804e24619a6105dcf1a418c396e35853f599
Bill Hannah <bill.hannah@gmail.com>
2012-05-07 00:00:28 +1200

    updated slides

--- js/impress.js
+++ js/impress.js
@@ -43,14 +43,14 @@
                 
                 memory[ prop ] = null;
                 for ( var i in props ) {
-                    if ( style[ props[i] ] !== undefined ) {
+                    if ( prop.hasOwnProperty(i) && style[ props[i] ] !== undefined ) {
                         memory[ prop ] = props[i];
                         break;
                     }
                 }
-            
+
             }
-            
+
             return memory[ prop ];
         };
     
@@ -218,7 +218,7 @@
     // for a presentation based on the element with given id ('impress'
     // by default).
     var impress = window.impress = function ( rootId ) {
-        
+
         // If impress.js is not supported by the browser return a dummy API
         // it may not be a perfect solution but we return early and avoid
         // running code that may use features not implemented in the browser.
@@ -230,38 +230,38 @@
                 next: empty
             };
         }
-        
+
         rootId = rootId || "impress";
-        
+
         // if given root is already initialized just return the API
         if (roots["impress-root-" + rootId]) {
             return roots["impress-root-" + rootId];
         }
-        
+
         // data of all presentation steps
         var stepsData = {};
-        
+
         // element of currently active step
         var activeStep = null;
-        
+
         // current state (position, rotation and scale) of the presentation
         var currentState = null;
-        
+
         // array of step elements
         var steps = null;
-        
+
         // configuration options
         var config = null;
-        
+
         // scale factor of the browser window
-        var windowScale = null;        
-        
+        var windowScale = null;
+
         // root presentation elements
         var root = byId( rootId );
         var canvas = document.createElement("div");
-        
+
         var initialized = false;
-        
+
         // STEP EVENTS
         //
         // There are currently two step events triggered by impress.js
@@ -269,10 +269,10 @@
         // screen (the transition from the previous one is finished) and
         // `impress:stepleave` is triggered when the step is left (the
         // transition to next step just starts).
-        
+
         // reference to last entered step
         var lastEntered = null;
-        
+
         // `onStepEnter` is called whenever the step element is entered
         // but the event is triggered only if the step is different than
         // last entered step.
@@ -282,7 +282,7 @@
                 lastEntered = step;
             }
         };
-        
+
         // `onStepLeave` is called whenever the step element is left
         // but the event is triggered only if the step is the same as
         // last entered step.
@@ -292,7 +292,7 @@
                 lastEntered = null;
             }
         };
-        
+
         // `initStep` initializes given step element by reading data from its
         // data attributes and setting correct styles.
         var initStep = function ( el, idx ) {
@@ -311,13 +311,13 @@
                     scale: toNumber(data.scale, 1),
                     el: el
                 };
-            
+
             if ( !el.id ) {
                 el.id = "step-" + (idx + 1);
             }
-            
+
             stepsData["impress-" + el.id] = step;
-            
+
             css(el, {
                 position: "absolute",
                 transform: "translate(-50%,-50%)" +
@@ -327,11 +327,11 @@
                 transformStyle: "preserve-3d"
             });
         };
-        
+
         // `init` API function that initializes (and runs) the presentation.
         var init = function () {
             if (initialized) { return; }
-            
+
             // First we set up the viewport for mobile devices.
             // For some reason iPad goes nuts when it is not done properly.
             var meta = $("meta[name='viewport']") || document.createElement("meta");
@@ -340,41 +340,41 @@
                 meta.name = 'viewport';
                 document.head.appendChild(meta);
             }
-            
+
             // initialize configuration object
             var rootData = root.dataset;
             config = {
                 width: toNumber( rootData.width, defaults.width ),
                 height: toNumber( rootData.height, defaults.height ),
                 maxScale: toNumber( rootData.maxScale, defaults.maxScale ),
-                minScale: toNumber( rootData.minScale, defaults.minScale ),                
+                minScale: toNumber( rootData.minScale, defaults.minScale ),
                 perspective: toNumber( rootData.perspective, defaults.perspective ),
                 transitionDuration: toNumber( rootData.transitionDuration, defaults.transitionDuration )
             };
-            
+
             windowScale = computeWindowScale( config );
-            
+
             // wrap steps with "canvas" element
             arrayify( root.childNodes ).forEach(function ( el ) {
                 canvas.appendChild( el );
             });
             root.appendChild(canvas);
-            
+
             // set initial styles
             document.documentElement.style.height = "100%";
-            
+
             css(body, {
                 height: "100%",
                 overflow: "hidden"
             });
-            
+
             var rootStyles = {
                 position: "absolute",
                 transformOrigin: "top left",
                 transition: "all 0s ease-in-out",
                 transformStyle: "preserve-3d"
             };
-            
+
             css(root, rootStyles);
             css(root, {
                 top: "50%",
@@ -382,26 +382,26 @@
                 transform: perspective( config.perspective/windowScale ) + scale( windowScale )
             });
             css(canvas, rootStyles);
-            
+
             body.classList.remove("impress-disabled");
             body.classList.add("impress-enabled");
-            
+
             // get and init steps
             steps = $$(".step", root);
             steps.forEach( initStep );
-            
+
             // set a default initial state of the canvas
             currentState = {
                 translate: { x: 0, y: 0, z: 0 },
                 rotate:    { x: 0, y: 0, z: 0 },
                 scale:     1
             };
-            
+
             initialized = true;
-            
+
             triggerEvent(root, "impress:init", { api: roots[ "impress-root-" + rootId ] });
         };
-        
+
         // `getStep` is a helper function that returns a step element defined by parameter.
         // If a number is given, step with index given by the number is returned, if a string
         // is given step element with such id is returned, if DOM element is given it is returned
@@ -414,19 +414,19 @@
             }
             return (step && step.id && stepsData["impress-" + step.id]) ? step : null;
         };
-        
+
         // used to reset timeout for `impress:stepenter` event
         var stepEnterTimeout = null;
-        
+
         // `goto` API function that moves to step given with `el` parameter (by index, id or element),
         // with a transition `duration` optionally given as second parameter.
         var goto = function ( el, duration ) {
-            
+
             if ( !initialized || !(el = getStep(el)) ) {
                 // presentation not initialized or given element is not a step
                 return false;
             }
-            
+
             // Sometimes it's possible to trigger focus on first link with some keyboard action.
             // Browser in such a case tries to scroll the page to make this element visible
             // (even that body overflow is set to hidden) and it breaks our careful positioning.
@@ -436,17 +436,17 @@
             //
             // If you are reading this and know any better way to handle it, I'll be glad to hear about it!
             window.scrollTo(0, 0);
-            
+
             var step = stepsData["impress-" + el.id];
-            
+
             if ( activeStep ) {
                 activeStep.classList.remove("active");
                 body.classList.remove("impress-on-" + activeStep.id);
             }
             el.classList.add("active");
-            
+
             body.classList.add("impress-on-" + el.id);
-            
+
             // compute target state of the canvas based on given step
             var target = {
                 rotate: {
@@ -461,7 +461,7 @@
                 },
                 scale: 1 / step.scale
             };
-            
+
             // Check if the transition is zooming in or not.
             //
             // This information is used to alter the transition style:
@@ -469,23 +469,23 @@
             // and the scaling is delayed, but when we are zooming out we start
             // with scaling down and move and rotation are delayed.
             var zoomin = target.scale >= currentState.scale;
-            
+
             duration = toNumber(duration, config.transitionDuration);
             var delay = (duration / 2);
-            
+
             // if the same step is re-selected, force computing window scaling,
             // because it is likely to be caused by window resize
             if (el === activeStep) {
                 windowScale = computeWindowScale(config);
             }
-            
+
             var targetScale = target.scale * windowScale;
-            
+
             // trigger leave of currently active element (if it's not the same step again)
             if (activeStep && activeStep !== el) {
                 onStepLeave(activeStep);
             }
-            
+
             // Now we alter transforms of `root` and `canvas` to trigger transitions.
             //
             // And here is why there are two elements: `root` and `canvas` - they are
@@ -501,13 +501,13 @@
                 transitionDuration: duration + "ms",
                 transitionDelay: (zoomin ? delay : 0) + "ms"
             });
-            
+
             css(canvas, {
                 transform: rotate(target.rotate, true) + translate(target.translate),
                 transitionDuration: duration + "ms",
                 transitionDelay: (zoomin ? 0 : delay) + "ms"
             });
-            
+
             // Here is a tricky part...
             //
             // If there is no change in scale or no change in rotation and translation, it means there was actually
@@ -523,17 +523,17 @@
                  currentState.translate.y === target.translate.y && currentState.translate.z === target.translate.z) ) {
                 delay = 0;
             }
-            
+
             // store current state
             currentState = target;
             activeStep = el;
-            
+
             // And here is where we trigger `impress:stepenter` event.
             // We simply set up a timeout to fire it taking transition duration (and possible delay) into account.
             //
             // I really wanted to make it in more elegant way. The `transitionend` event seemed to be the best way
             // to do it, but the fact that I'm using transitions on two separate elements and that the `transitionend`
-            // event is only triggered when there was a transition (change in the values) caused some bugs and 
+            // event is only triggered when there was a transition (change in the values) caused some bugs and
             // made the code really complicated, cause I had to handle all the conditions separately. And it still
             // needed a `setTimeout` fallback for the situations when there is no transition at all.
             // So I decided that I'd rather make the code simpler than use shiny new `transitionend`.
@@ -544,26 +544,26 @@
             stepEnterTimeout = window.setTimeout(function() {
                 onStepEnter(activeStep);
             }, duration + delay);
-            
+
             return el;
         };
-        
+
         // `prev` API function goes to previous step (in document order)
         var prev = function () {
             var prev = steps.indexOf( activeStep ) - 1;
             prev = prev >= 0 ? steps[ prev ] : steps[ steps.length-1 ];
-            
+
             return goto(prev);
         };
-        
+
         // `next` API function goes to next step (in document order)
         var next = function () {
             var next = steps.indexOf( activeStep ) + 1;
             next = next < steps.length ? steps[ next ] : steps[ 0 ];
-            
+
             return goto(next);
         };
-        
+
         // Adding some useful classes to step elements.
         //
         // All the steps that have not been shown yet are given `future` class.
@@ -582,26 +582,26 @@
             steps.forEach(function (step) {
                 step.classList.add("future");
             });
-            
+
             root.addEventListener("impress:stepenter", function (event) {
                 event.target.classList.remove("past");
                 event.target.classList.remove("future");
                 event.target.classList.add("present");
             }, false);
-            
+
             root.addEventListener("impress:stepleave", function (event) {
                 event.target.classList.remove("present");
                 event.target.classList.add("past");
             }, false);
-            
+
         }, false);
-        
+
         // Adding hash change support.
         root.addEventListener("impress:init", function(){
-            
+
             // last hash detected
             var lastHash = "";
-            
+
             // `#/step-id` is used instead of `#step-id` to prevent default browser
             // scrolling to element in hash.
             //
@@ -611,7 +611,7 @@
             root.addEventListener("impress:stepenter", function (event) {
                 window.location.hash = lastHash = "#/" + event.target.id;
             }, false);
-            
+
             window.addEventListener("hashchange", function () {
                 // When the step is entered hash in the location is updated
                 // (just few lines above from here), so the hash change is 
@@ -622,14 +622,14 @@
                     goto( getElementFromHash() );
                 }
             }, false);
-            
+
             // START 
             // by selecting step defined in url or first step of the presentation
             goto(getElementFromHash() || steps[0], 0);
         }, false);
-        
+
         body.classList.add("impress-disabled");
-        
+
         // store and return API for given impress.js root element
         return (roots[ "impress-root-" + rootId ] = {
             init: init,
